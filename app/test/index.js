32ff19b803f852c52b8abbe7308b0a05fa4b9666
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-01 23:04:14 +0200

    :bug: Fix WCT tests implementation
    Wondering why all your tests passed?

---
+++ app/test/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+WCT.loadSuites([
+  'td-model.html',
+  'td-model.html?dom=shadow'
+]);
--- app/test/td-model.js
+++ app/test/td-model.js
@@ -2,28 +2,28 @@
  * Tests are written in ES6
  */
 
-suite('td-model tests',  () => {
+suite('td-model tests', () => {
   'use strict';
 
   let tdModel,
     ls;
 
-  setup( () => {
+  setup(() => {
     tdModel = fixture('model');
     ls = tdModel.$$('iron-localstorage');
   });
 
-  test('Has hidden attribute',  () => {
+  test('Has hidden attribute', () => {
     assert.isTrue(tdModel.hidden, 'The element is hidden');
   });
 
-  test('Test child iron-localstorage',  () => {
+  test('Test child iron-localstorage', () => {
     assert.isNotNull(ls, 'There is a local storage component');
   });
 
   test('Has items', (done) => {
     // when loaded it creates default item array
-    ls.addEventListener('iron-localstorage-load-empty',  () => {
+    ls.addEventListener('iron-localstorage-load-empty', () => {
       assert.isOk(tdModel.items, 'Has items');
       done();
     });
