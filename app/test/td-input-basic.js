8ae74f948035731b2219435648b72e096598e3cd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-04 00:04:51 +0200

    td-input impl and tests cont.
    - events/mocks

--- app/test/td-input-basic.js
+++ app/test/td-input-basic.js
@@ -10,4 +10,14 @@ suite('td-input tests', () => {
     assert.equal(input.tagName.toLowerCase(), 'input');
   });
 
+  test('brodcasts td-input-commit event on ENTER', () => {
+    let enterKeySpy = sinon.spy();
+    input.addEventListener('td-input-commit', enterKeySpy);
+    input.focus();
+    assert.equal(input, document.activeElement, 'input has focus');
+    MockInteractions.pressEnter(input);
+    assert.isTrue(enterKeySpy.called, 'the event was fired');
+    input.removeEventListener('td-input-commit', enterKeySpy);
+  });
+
 });
