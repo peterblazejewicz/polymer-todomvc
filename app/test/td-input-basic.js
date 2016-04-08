8f9b779d708780d745b90b8d4bc47ca0ffd73893
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-08 23:51:11 +0200

    Fixing test

--- app/test/td-input-basic.js
+++ app/test/td-input-basic.js
@@ -10,4 +10,19 @@ suite('td-input tests', () => {
     assert.equal(input.tagName.toLowerCase(), 'input');
   });
 
+  test('native keydown event', () => {
+    let enterKeySpy = sinon.spy();
+    input.addEventListener('keydown', enterKeySpy);
+    MockInteractions.pressEnter(input);
+    assert.isTrue(enterKeySpy.called, 'the event was fired');
+    input.removeEventListener('keydown', enterKeySpy);
+  });
+
+  test('td-input-commit event', () => {
+    input.addEventListener('td-input-commit', (event) => {
+       assert.isTrue(true, 'td-input-commit fired');
+    });
+    MockInteractions.pressEnter(input);
+  });
+
 });
